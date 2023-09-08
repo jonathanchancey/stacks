9dab848c7d957a3f662cd18d465ebf0ec13c5d43
Jonathan Chancey <jonathan22711@gmail.com>
2023-09-07 20:59:33 -0700

    undo everything and take it back

--- acolyte.tf
+++ acolyte.tf
@@ -26,7 +26,7 @@ resource "proxmox_vm_qemu" "acolyte" {
     os_type = "cloud-init"
     ipconfig0 = "ip=192.168.0.30,gw=192.168.0.1"
     nameserver = "192.168.0.3"
-    ciuser = "kube"
+    ssh_user = "root"
     sshkeys = <<EOF
     ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDFr6EdUZiVh5LIAZgukLDp93gj/ktEyoNUjcE8QSG3Zovf9VABlxkPY9f7KnQaAB3AeMhEIWYpZbHkMgF2l3TMuhPwp7VABds9lLHdb0YuZYL4Y7peRRhNSF6oDOetOkFt46nTs123aChCfj8UrhIR04PqP//LCBQ/gsS+XEDH4jlYGXCtEfOHgwHNFZPwu9aRA+xTj/5OWxr8e8Qtas8RFxt26Q99iHJRiCQIIckR/mObkvy9+09r04fpcfxCPADA/AvvOBRvdmjD3Pd6WP8GnX8DcAGXsUNYDQPr4dAGh0qV/uHomH16c+bIzygUO1FVEFHfLZUhyvN+UjIVrX+iwlXCs+Juf18a/1mJ/DHK+XpckozGowfBXPD8lvhzU7KCG8BvinZ/UvMUbe5aUsFsCsb60yZ/7Nx9ptcsSN1n0jILm6B7mw3rzWQcmQmErMGq/z7xuqeAklHGc5GuVIVzJ+NzPyiDHjQblxYE0iiTpCv0xdIDnl51/+EDOJhJcjc= seki@yu
     EOF 
