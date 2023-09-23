7b85abf5bf2afd3eeee5466b9363e2e1399ed42d
Jonathan Chancey <jonathan@chancey.dev>
2023-09-22 18:15:11 -0700

    add initial repeated config example

--- terraform/examples/k3s/main.tf
+++ terraform/examples/k3s/main.tf
@@ -1,10 +1,58 @@
-
-
-module "proxmox_vm_example1" {
+module "acolyte-00" {
   source                   = "../../modules/proxmox-qemu" # Adjust the path as needed
-  sshkeys                  = var.sshkeys
   name                     = "acolyte-00"
   target_node              = "shar"
+  sshkeys                  = var.sshkeys
+  proxmox_api_url          = var.proxmox_api_url
+  proxmox_api_token_id     = var.proxmox_api_token_id
+  proxmox_api_token_secret = var.proxmox_api_token_secret
+}
+
+module "acolyte-01" {
+  source                   = "../../modules/proxmox-qemu" # Adjust the path as needed
+  name                     = "acolyte-01"
+  target_node              = "selune"
+  sshkeys                  = var.sshkeys
+  proxmox_api_url          = var.proxmox_api_url
+  proxmox_api_token_id     = var.proxmox_api_token_id
+  proxmox_api_token_secret = var.proxmox_api_token_secret
+}
+
+module "acolyte-02" {
+  source                   = "../../modules/proxmox-qemu" # Adjust the path as needed
+  name                     = "acolyte-02"
+  target_node              = "okapi"
+  sshkeys                  = var.sshkeys
+  proxmox_api_url          = var.proxmox_api_url
+  proxmox_api_token_id     = var.proxmox_api_token_id
+  proxmox_api_token_secret = var.proxmox_api_token_secret
+}
+
+module "neophyte-00" {
+  source                   = "../../modules/proxmox-qemu" # Adjust the path as needed
+  name                     = "neophyte-00"
+  target_node              = "shar"
+  sshkeys                  = var.sshkeys
+  proxmox_api_url          = var.proxmox_api_url
+  proxmox_api_token_id     = var.proxmox_api_token_id
+  proxmox_api_token_secret = var.proxmox_api_token_secret
+}
+
+module "neophyte-01" {
+  source                   = "../../modules/proxmox-qemu" # Adjust the path as needed
+  name                     = "neophyte-01"
+  target_node              = "selune"
+  sshkeys                  = var.sshkeys
+  proxmox_api_url          = var.proxmox_api_url
+  proxmox_api_token_id     = var.proxmox_api_token_id
+  proxmox_api_token_secret = var.proxmox_api_token_secret
+}
+
+module "neophyte-02" {
+  source                   = "../../modules/proxmox-qemu" # Adjust the path as needed
+  name                     = "neophyte-02"
+  target_node              = "okapi"
+  sshkeys                  = var.sshkeys
   proxmox_api_url          = var.proxmox_api_url
   proxmox_api_token_id     = var.proxmox_api_token_id
   proxmox_api_token_secret = var.proxmox_api_token_secret
