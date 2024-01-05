0c0e2bf9ba060aa4bb1e9a6a7b0d0dfc599be596
Jonathan Chancey <jonathan@chancey.dev>
2024-01-04 22:23:10 -0800

    add start of bastille

--- terraform/bastille/main.tf
+++ terraform/bastille/main.tf
@@ -1,13 +1,22 @@
 resource "proxmox_virtual_environment_vm" "microos_vm" {
-  name      = "test-MicroOS"
+  name      = "sentinel-01"
   node_name = "lich"
+  description = "Managed by Terraform"
 
   initialization {
     datastore_id = "hydra"
     user_account {
       # do not use this in production, configure your own ssh key instead!
-      username = "root"
+      username = var.vm_username
       password = var.vm_password
+    }
+    dns {
+      servers = ["1.1.1.1"]
+    }
+    ip_config {
+      ipv4 {
+        address = "dhcp"
+      }
     }
   }
 
@@ -19,6 +28,24 @@ resource "proxmox_virtual_environment_vm" "microos_vm" {
     discard      = "on"
     size         = 10
   }
+
+  network_device {
+    vlan_id = 30
+  }
+
+  # connection {
+  #   type        = "ssh"
+  #   agent       = false
+  #   host        = element(element(self.ipv4_addresses, index(self.network_interface_names, "eth0")), 0)
+  #   private_key = tls_private_key.example.private_key_pem
+  #   user        = "root"
+  # }
+
+  # provisioner "remote-exec" {
+  #   inline = [
+  #     "echo Welcome to $(hostname)!",
+  #   ]
+  # }
 }
 
 resource "proxmox_virtual_environment_file" "microos_cloud_image" {
@@ -28,7 +55,6 @@ resource "proxmox_virtual_environment_file" "microos_cloud_image" {
 
   source_file {
     # you may download this image locally on your workstation and then use the local path instead of the remote URL
-    # path      = "https://download.opensuse.org/tumbleweed/appliances/openSUSE-MicroOS.x86_64-16.0.0-ContainerHost-kvm-and-xen-Snapshot20240103.qcow2"
     path      = "https://download.opensuse.org/tumbleweed/appliances/openSUSE-MicroOS.x86_64-16.0.0-ContainerHost-OpenStack-Cloud-Snapshot20240103.qcow2"
     file_name = "openSUSE-MicroOS-cloudstack.img"
 
