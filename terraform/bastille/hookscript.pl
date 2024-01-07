3f2c105a3cb5d47f315a0abde2218e93b5c4a7ad
Jonathan Chancey <jonathan@chancey.dev>
2024-01-06 19:56:35 -0800

    update hookscript

--- terraform/bastille/hookscript.pl
+++ terraform/bastille/hookscript.pl
@@ -1,24 +1,14 @@
 #!/usr/bin/perl
 
-# Exmple hook script for PVE guests (hookscript config option)
-# You can set this via pct/qm with
-# pct set <vmid> -hookscript <volume-id>
-# qm set <vmid> -hookscript <volume-id>
-# where <volume-id> has to be an executable file in the snippets folder
-# of any storage with directories e.g.:
-# qm set 100 -hookscript local:snippets/hookscript.pl
-
 use strict;
 use warnings;
 
 print "GUEST HOOK: " . join(' ', @ARGV). "\n";
 
 # First argument is the vmid
-
 my $vmid = shift;
 
 # Second argument is the phase
-
 my $phase = shift;
 
 if ($phase eq 'post-start') {
@@ -27,6 +17,10 @@ if ($phase eq 'post-start') {
     # successfully started.
 
     print "$vmid started successfully.\n";
+
+    my $bash_script = '/mnt/pve/chimera/snippets/hook_script_server.sh';
+
+    system("bash $bash_script");
 
 } else {
     die "got unknown phase '$phase'\n";
