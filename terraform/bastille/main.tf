fc52b7e74062cb86ea8964ca1037246bf8dc48f1
Jonathan Chancey <jonathan@chancey.dev>
2024-01-08 12:46:55 -0800

    switch to ubuntu 22.04 to troubleshoot
    lo and behold, things works

--- terraform/bastille/main.tf
+++ terraform/bastille/main.tf
@@ -1,5 +1,5 @@
-resource "proxmox_virtual_environment_vm" "microos_template" {
-  name        = "MicroOS-template"
+resource "proxmox_virtual_environment_vm" "ubuntu_template" {
+  name        = "ubuntu-template"
   node_name   = "lich"
   description = "Managed by Terraform"
 
@@ -25,11 +25,11 @@ resource "proxmox_virtual_environment_vm" "microos_template" {
 
   disk {
     datastore_id = "hydra"
-    file_id      = proxmox_virtual_environment_file.microos_cloud_image.id
+    file_id      = proxmox_virtual_environment_file.ubuntu_cloud_image.id
     interface    = "virtio0"
     iothread     = true
     discard      = "on"
-    size         = 24
+    size         = 20
   }
 
   network_device {
@@ -51,7 +51,7 @@ resource "proxmox_virtual_environment_vm" "sentinel-01" {
   vm_id               = 20001
 
   clone {
-    vm_id = proxmox_virtual_environment_vm.microos_template.id
+    vm_id = proxmox_virtual_environment_vm.ubuntu_template.id
   }
 
   memory {
@@ -79,7 +79,7 @@ resource "proxmox_virtual_environment_vm" "cavalier-01" {
   vm_id       = 20101
 
   clone {
-    vm_id = proxmox_virtual_environment_vm.microos_template.id
+    vm_id = proxmox_virtual_environment_vm.ubuntu_template.id
   }
 
   memory {
@@ -89,6 +89,7 @@ resource "proxmox_virtual_environment_vm" "cavalier-01" {
   cpu {
     cores = 2
   }
+
   initialization {
     ip_config {
       ipv4 {
@@ -99,15 +100,15 @@ resource "proxmox_virtual_environment_vm" "cavalier-01" {
   }
 }
 
-resource "proxmox_virtual_environment_file" "microos_cloud_image" {
+resource "proxmox_virtual_environment_file" "ubuntu_cloud_image" {
   content_type = "iso"
   datastore_id = "local"
   node_name    = "lich"
 
   source_file {
     # you may download this image locally on your workstation and then use the local path instead of the remote URL
-    path      = "https://download.opensuse.org/tumbleweed/appliances/openSUSE-MicroOS.x86_64-16.0.0-ContainerHost-OpenStack-Cloud-Snapshot20240106.qcow2"
-    file_name = "openSUSE-MicroOS-cloudstack.img"
+    path      = "https://cloud-images.ubuntu.com/releases/22.04/release/ubuntu-22.04-server-cloudimg-amd64.img"
+    file_name = "ubuntu-22.04.img"
 
     # you may also use the SHA256 checksum of the image to verify its integrity
     # checksum = "a370d8e6141e5359ca865c29cc8b6d95926b0c162e906453e388ccf24d353b6b"
