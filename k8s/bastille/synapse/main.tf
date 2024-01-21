8f630c9dc18e84b63e5eae474b7faa4309371af1
Jonathan Chancey <jonathan@chancey.dev>
2024-01-21 11:44:32 -0800

    add synapse readme and e2-micro tf

---
+++ k8s/bastille/synapse/main.tf
@@ -0,0 +1,64 @@
+# This code is compatible with Terraform 4.25.0 and versions that are backwards compatible to 4.25.0.
+# For information about validating this Terraform code, see https://developer.hashicorp.com/terraform/tutorials/gcp-get-started/google-cloud-platform-build#format-and-validate-the-configuration
+
+resource "google_compute_instance" "turn-firefly" {
+  boot_disk {
+    auto_delete = true
+    device_name = "instance-1"
+
+    initialize_params {
+      image = "projects/debian-cloud/global/images/debian-11-bullseye-v20240110"
+      size  = 10
+      type  = "pd-standard"
+    }
+
+    mode = "READ_WRITE"
+  }
+
+  can_ip_forward      = true
+  deletion_protection = false
+  enable_display      = false
+
+  labels = {
+    goog-ec-src = "vm_add-tf"
+  }
+
+  machine_type = "e2-micro"
+
+  metadata = {
+  }
+
+  name = "turn-firefly"
+
+  network_interface {
+    access_config {
+      nat_ip       = "REDACTED"
+      network_tier = "STANDARD"
+    }
+
+    queue_count = 0
+    stack_type  = "IPV4_ONLY"
+    subnetwork  = "projects/turn-firefly/regions/us-central1/subnetworks/default"
+  }
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+    preemptible         = false
+    provisioning_model  = "STANDARD"
+  }
+
+  service_account {
+    email  = "REDACTED"
+    scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
+  }
+
+  shielded_instance_config {
+    enable_integrity_monitoring = true
+    enable_secure_boot          = false
+    enable_vtpm                 = true
+  }
+
+  tags = ["http-server", "https-server"]
+  zone = "us-central1-a"
+}
