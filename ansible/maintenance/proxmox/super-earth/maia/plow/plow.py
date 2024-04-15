fa32bd701a1b79d68d7f701af398a749fe84ddcb
Jonathan Chancey <jonathan@chancey.dev>
2024-04-15 09:47:10 -0700

    add plow dir

--- ansible/maintenance/proxmox/super-earth/maia/plow/plow.py
+++ ansible/maintenance/proxmox/super-earth/maia/plow/plow.py
@@ -28,7 +28,7 @@ SOURCES = glob.glob('/mnt/space/plots/')
 # DESTS = ["chia@10.10.0.227::plots/3242", "chia@10.10.0.227::plots/7f21", "chia@10.10.0.227::plots/cd07", "chia@10.10.0.227::plots/ddd2", "chia@10.10.0.227::plots/8a48"]
 
 # PLOT_DIRS = "/22a5:/3242:/621a:/7f21:/a1e8:/cd07:/ddd2:/345d".split(sep=':')
-PLOT_DIRS = "/7f21".split(sep=':')
+PLOT_DIRS = "/7f21:/345d".split(sep=':')
 
 DESTS = [f"chia@10.10.0.227::plots{plotdir}" for plotdir in PLOT_DIRS]
 
