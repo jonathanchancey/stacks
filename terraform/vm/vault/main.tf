28bcd8e963fb0ca560dc90690c91a4a2112fbe97
jonathanchancey <jonathan@chancey.dev>
2024-08-02 18:59:06 -0700

    add vault vm and locks

---
+++ terraform/vm/vault/main.tf
@@ -0,0 +1,30 @@
+module "miller" {
+  source                       = "../../modules/proxmox-vm"
+  name                         = "miller"
+  description                  = "Managed by Terraform"
+  tags                         = ["debian", "terraform"]
+  node_name                    = "TARS"
+  cloud_image_node_name        = "TARS"
+  datastore_id                 = "cornfield"
+  cloud_image_datastore_id     = "local"
+  vm_id                        = 130
+  memory_dedicated             = 4096
+  cpu_cores                    = 4
+  disk_size                    = 20
+  ip_config_ipv6_address       = "dhcp"
+  ip_config_ipv6_gateway       = var.ip_config_ipv6_gateway
+  dns_domain                   = "local"
+  dns_servers                  = var.dns_servers
+  sshkeys                      = var.sshkeys
+  username                     = var.username
+  password                     = var.password
+  reboot                       = false
+  cloud_image_url              = "https://cloud.debian.org/images/cloud/bookworm/20240717-1811/debian-12-generic-amd64-20240717-1811.qcow2"
+  cloud_image_file_name        = "debian-12-generic-amd64-20240717-1811.img"
+  cloud_image_checksum         = ""
+  virtual_environment_endpoint = var.virtual_environment_endpoint
+  virtual_environment_password = var.virtual_environment_password
+  virtual_environment_username = var.virtual_environment_username
+  tpm_state_datastore_id       = "local"
+  network_device_vlan_id       = 131
+}
