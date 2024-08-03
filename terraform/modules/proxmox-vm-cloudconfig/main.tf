f976663a5f399e4585985104c5774e82002a6142
jonathanchancey <jonathan@chancey.dev>
2024-08-02 19:04:12 -0700

    add draft of cloudconfig with tf variables

---
+++ terraform/modules/proxmox-vm-cloudconfig/main.tf
@@ -0,0 +1,139 @@
+resource "proxmox_virtual_environment_vm" "vm" {
+  name        = var.name
+  node_name   = var.node_name
+  description = var.description
+  tags        = var.tags
+  vm_id       = var.vm_id
+
+  initialization {
+    datastore_id = var.datastore_id
+
+    user_data_file_id = proxmox_virtual_environment_file.cloud_config.id
+  }
+
+  network_device {
+    vlan_id = var.network_device_vlan_id
+  }
+
+  disk {
+    datastore_id = var.datastore_id
+    file_id      = proxmox_virtual_environment_file.cloud_image.id
+    interface    = "virtio0"
+    iothread     = true
+    discard      = "on"
+    size         = var.disk_size
+  }
+
+  // Dynamic block for optional additional disk
+  dynamic "disk" {
+    for_each = var.additional_disk_size > 0 ? [1] : []
+    content {
+      datastore_id = var.additional_disk_datastore_id
+      file_format  = var.additional_disk_file_format
+      interface    = "virtio1"
+      iothread     = true
+      discard      = "on"
+      size         = var.additional_disk_size
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      disk[0].file_format,
+      disk[0].path_in_datastore
+    ]
+  }
+
+  tpm_state {
+    datastore_id = var.tpm_state_datastore_id
+    version = var.tpm_state
+  }
+
+  dynamic "clone" {
+    for_each = var.clone
+    content {
+      vm_id = var.clone_vm_id
+    }
+  }
+
+  memory {
+    dedicated = var.memory_dedicated
+  }
+
+  cpu {
+    cores = var.cpu_cores
+  }
+
+  template = var.template
+  reboot   = var.reboot
+}
+
+resource "proxmox_virtual_environment_file" "cloud_image" {
+  content_type = var.cloud_image_content_type
+  datastore_id = var.cloud_image_datastore_id
+  node_name    = var.cloud_image_node_name
+
+  source_file {
+    # you may download this image locally on your workstation and then use the local path instead of the remote URL
+    path      = var.cloud_image_url
+    file_name = var.cloud_image_file_name
+
+    # you may also use the SHA256 checksum of the image to verify its integrity
+    checksum = var.cloud_image_checksum
+  }
+}
+
+  # initialization {
+  #   datastore_id = var.datastore_id
+  #   user_account {
+  #     username = var.username
+  #     password = var.password
+  #     keys     = var.sshkeys
+  #   }
+
+  #   dns {
+  #     domain  = var.dns_domain
+  #     servers = var.dns_servers
+  #   }
+
+  #   user_data_file_id = proxmox_virtual_environment_file.cloud_config.id
+
+  #   ip_config {
+  #     ipv4 {
+  #       address = var.ip_config_ipv4
+  #       gateway = var.ip_config_gateway
+  #     }
+  #     ipv6 {
+  #       address = var.ip_config_ipv6_address
+  #       gateway = var.ip_config_ipv6_gateway
+  #     }
+  #   }
+  # }
+resource "proxmox_virtual_environment_file" "cloud_config" {
+  content_type = "snippets"
+  datastore_id = var.cloud_image_datastore_id
+  node_name    = var.cloud_image_node_name
+
+  source_raw {
+    data = <<-EOF
+    #cloud-config
+    chpasswd:
+      list: |
+        ${var.username}:${var.password}
+      expire: false
+    hostname: ${var.name}
+    packages:
+      - qemu-guest-agent
+    users:
+      - default
+      - name: ${var.username}
+        groups: sudo
+        shell: /bin/bash
+        ssh-authorized-keys:
+          - ${trimspace(tls_private_key.example.public_key_openssh)}
+        sudo: ALL=(ALL) NOPASSWD:ALL
+    EOF
+
+    file_name = "cloud-config.yaml"
+  }
+}
---
+++ terraform/modules/proxmox-vm-cloudconfig/variables.tf
@@ -0,0 +1,184 @@
+variable "virtual_environment_endpoint" {
+  type        = string
+  description = "The endpoint for the Proxmox Virtual Environment API (example: https://host:port)"
+}
+
+variable "virtual_environment_password" {
+  type        = string
+  description = "The password for the Proxmox Virtual Environment API"
+}
+
+variable "virtual_environment_username" {
+  type        = string
+  description = "The username and realm for the Proxmox Virtual Environment API (example: root@pam)"
+}
+
+variable "name" {
+  type    = string
+  default = "vm name"
+}
+
+variable "node_name" {
+  type = string
+}
+
+variable "description" {
+  type    = string
+  default = "Managed by Terraform"
+}
+
+variable "tags" {
+  type    = list(string)
+  default = ["terraform"]
+}
+
+variable "vm_id" {
+  type    = number
+  default = null
+}
+
+variable "datastore_id" {
+  type      = string
+  default   = "local"
+  sensitive = true
+}
+
+variable "username" {
+  type = string
+}
+
+variable "password" {
+  type      = string
+  sensitive = true
+}
+
+variable "sshkeys" {
+  type        = list(string)
+  description = "sshkeys"
+}
+
+variable "dns_domain" {
+  type    = string
+  default = ""
+}
+
+variable "dns_servers" {
+  type    = list(string)
+  default = []
+}
+
+variable "ip_config_ipv4" {
+  type    = string
+  default = ""
+}
+
+variable "ip_config_gateway" {
+  type    = string
+  default = ""
+}
+
+variable "ip_config_ipv6_address" {
+  type    = string
+  default = ""
+}
+
+variable "ip_config_ipv6_gateway" {
+  type    = string
+  default = ""
+}
+
+variable "network_device_vlan_id" {
+  type    = number
+  default = 0
+}
+
+variable "disk_size" {
+  type    = number
+  default = 20
+}
+
+variable "tpm_state" {
+  type    = string
+  default = "v2.0"
+}
+
+variable "clone" {
+  type    = set(map(bool))
+  default = []
+}
+
+variable "clone_vm_id" {
+  type    = number
+  default = null
+}
+
+variable "memory_dedicated" {
+  type    = number
+  default = 4096
+}
+
+variable "cpu_cores" {
+  type    = number
+  default = 2
+}
+
+variable "template" {
+  type    = bool
+  default = false
+}
+
+variable "reboot" {
+  type    = bool
+  default = true
+}
+
+variable "cloud_image_content_type" {
+  type    = string
+  default = "iso"
+}
+
+variable "cloud_image_datastore_id" {
+  type    = string
+}
+
+variable "cloud_image_node_name" {
+  type = string
+}
+
+variable "cloud_image_checksum" {
+  type        = string
+  default     = ""
+  description = "SHA256 checksum of cloud image"
+}
+
+variable "cloud_image_url" {
+  type    = string
+}
+
+variable "cloud_image_file_name" {
+  type    = string
+}
+
+variable "additional_disk_size" {
+  type        = number
+  default     = 0
+  description = "Size of the additional disk"
+}
+
+variable "additional_disk_file_format" {
+  type        = string
+  default     = ""
+  description = "File format for the additional disk. May need to be raw for LVM"
+}
+
+variable "additional_disk_datastore_id" {
+  type        = string
+  default     = ""
+  description = "Datastore ID for the additional disk"
+}
+
+variable "tpm_state_datastore_id" {
+  type        = string
+  default     = "local-lvm"
+  description = "tpm_state_datastore_id"
+}
