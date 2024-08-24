204a2a60694dd4e35b746e957b262ecf49e12996
jonathanchancey <jonathan@chancey.dev>
2024-08-23 19:26:00 -0700

    add ipv4 output and output only if output exists

--- terraform/modules/proxmox-vm-cloudconfig/outputs.tf
+++ terraform/modules/proxmox-vm-cloudconfig/outputs.tf
@@ -12,3 +12,8 @@ output "vm_ipv6_addresses" {
   description = "The IPv6 address assigned to the VM via DHCP"
   value       = proxmox_virtual_environment_vm.vm.ipv6_addresses
 }
+
+output "vm_ipv4_addresses" {
+  description = "The IPv6 address assigned to the VM via DHCP"
+  value       = proxmox_virtual_environment_vm.vm.ipv4_addresses
+}
--- terraform/vm/pocket/outputs.tf
+++ terraform/vm/pocket/outputs.tf
@@ -10,5 +10,10 @@ output "vm_id" {
 
 output "vm_ipv6_address" {
   description = "The IPv6 address assigned to the VM via DHCP"
-  value       = { for k, v in module.vm : k => v.vm_ipv6_addresses[1][0] }
+  value       = { for k, v in module.vm : k => (length(v.vm_ipv6_addresses) > 1 && length(v.vm_ipv6_addresses[1]) > 0 ? v.vm_ipv6_addresses[1][0] : null) }
+}
+
+output "vm_ipv4_address" {
+  description = "The IPv6 address assigned to the VM via DHCP"
+  value       = { for k, v in module.vm : k => (length(v.vm_ipv4_addresses) > 1 && length(v.vm_ipv4_addresses[1]) > 0 ? v.vm_ipv4_addresses[1][0] : null) }
 }
