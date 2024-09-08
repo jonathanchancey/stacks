2eaa2374081264c7e2960e9d01b934c8841790c0
jonathanchancey <jonathan@chancey.dev>
2024-09-08 15:24:57 -0700

    add syncthing dns record

--- terraform/network/cloudflare/cloudflare.tf
+++ terraform/network/cloudflare/cloudflare.tf
@@ -4,3 +4,10 @@ resource "cloudflare_record" "navidrome" {
   content = var.traefik_ip
   type    = "A"
 }
+
+resource "cloudflare_record" "syncthing" {
+  zone_id = var.cloudflare_zone_id
+  name    = "syncthing"
+  content = var.traefik_ip
+  type    = "A"
+}
