55bbc7305453ff3c6fd34f06ba20f7e8d90d7863
jonathanchancey <jonathan@chancey.dev>
2024-09-08 15:18:55 -0700

    remove unused vms

--- terraform/vm/spot/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vm_name" {
-  description = "The name of the created Proxmox VM"
-  value       = module.spot.vm_name
-}
-
-output "vm_id" {
-  description = "The ID of the created Proxmox VM"
-  value       = module.spot.vm_id
-}
