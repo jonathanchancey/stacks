ca27848549969a3a46039bee3d44f45b25b6360c
jonathanchancey <jonathan@chancey.dev>
2024-11-23 17:03:27 -0800

    add lich-00 dns record

--- terraform/network/pihole-dns/main.tf
+++ terraform/network/pihole-dns/main.tf
@@ -7,3 +7,8 @@ resource "pihole_dns_record" "okapi_internal" {
   domain = "okapi.internal"
   ip     = "10.10.0.19"
 }
+
+resource "pihole_dns_record" "lich_internal" {
+  domain = "lich-00.internal"
+  ip     = "10.10.0.20"
+}
