baa45832555c5486d14604b618ccdf780ade78cd
Jonathan Chancey <jonathan@chancey.dev>
2026-01-16 14:55:04 -0800

    feat(task): speed up testing with ergonomics and limits (#309)

--- tools/scripts/kubeconform.sh
+++ tools/scripts/kubeconform.sh
@@ -9,36 +9,34 @@ KUBERNETES_DIR=$1
 kustomize_args=("--load-restrictor=LoadRestrictionsNone")
 kustomize_config="kustomization.yaml"
 kubeconform_args=(
-    "-strict"
-    "-ignore-missing-schemas"
-    "-skip"
-    "Secret"
-    "-schema-location"
-    "default"
-    "-schema-location"
-    "https://kubernetes-schemas.pages.dev/{{.Group}}/{{.ResourceKind}}_{{.ResourceAPIVersion}}.json"
-    "-verbose"
+  "-strict"
+  "-ignore-missing-schemas"
+  "-skip"
+  "Secret"
+  "-schema-location"
+  "default"
+  "-schema-location"
+  "https://kubernetes-schemas.pages.dev/{{.Group}}/{{.ResourceKind}}_{{.ResourceAPIVersion}}.json"
+  # "-verbose"
 )
 
 echo "=== Validating standalone manifests in ${KUBERNETES_DIR} ==="
-find "${KUBERNETES_DIR}" -maxdepth 1 -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file;
-do
-    kubeconform "${kubeconform_args[@]}" "${file}"
-    if [[ ${PIPESTATUS[0]} != 0 ]]; then
-        exit 1
-    fi
+find "${KUBERNETES_DIR}" -maxdepth 1 -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file; do
+  kubeconform "${kubeconform_args[@]}" "${file}"
+  if [[ ${PIPESTATUS[0]} != 0 ]]; then
+    exit 1
+  fi
 done
 
 echo "=== Validating kustomizations in ${KUBERNETES_DIR} ==="
-find "${KUBERNETES_DIR}" -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file;
-do
-    # Skip component templates that contain variable placeholders
-    if [[ "${file}" == *"/components/"* ]]; then
-        continue
-    fi
-    echo "=== Validating kustomizations in ${file/%$kustomize_config} ==="
-    kustomize build "${file/%$kustomize_config}" "${kustomize_args[@]}" | kubeconform "${kubeconform_args[@]}"
-    if [[ ${PIPESTATUS[0]} != 0 ]]; then
-        exit 1
-    fi
+find "${KUBERNETES_DIR}" -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file; do
+  # Skip component templates that contain variable placeholders
+  if [[ "${file}" == *"/components/"* ]]; then
+    continue
+  fi
+  echo "=== Validating kustomizations in ${file/%$kustomize_config/} ==="
+  kustomize build "${file/%$kustomize_config/}" "${kustomize_args[@]}" | kubeconform "${kubeconform_args[@]}"
+  if [[ ${PIPESTATUS[0]} != 0 ]]; then
+    exit 1
+  fi
 done
